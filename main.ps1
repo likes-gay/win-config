39b508c0a0f23655af0bb75740fa9a376ad61284
Luke Thomas <96008479+Zoobdude@users.noreply.github.com>
2024-04-25 11:02:15 +0000

    add new features, most noteably edge redirect
    Co-authored-by: Yummy_Bacon5 <YummyBacon5@users.noreply.github.com>

--- main.ps1
+++ main.ps1
@@ -1,7 +1,13 @@
+#functions
 function UnPin-App { param(
-		[string]$appname
-	)
-	((New-Object -Com Shell.Application).NameSpace("shell:::{4234d49b-0245-4df3-b780-3893943456e1}").Items() | ?{$_.Name -eq $appname}).Verbs() | ?{$_.Name.replace("&","") -match "Unpin from taskbar"} | %{$_.DoIt()}
+	[string]$appname
+)
+try {
+	((New-Object -Com Shell.Application).NameSpace("shell:::{4234d49b-0245-4df3-b780-3893943456e1}").Items() | ?{$_.Name -eq $appname}).Verbs() | ?{$_.Name.replace("&", "") -match "Unpin from taskbar"} | %{$_.DoIt()}
+	return "App '$appname' unpinned from Taskbar"
+} catch {
+	Write-Error "Error Unpinning App! (App-Name correct?)"
+}
 }
 
 UnPin-App "Microsoft Edge"
@@ -13,34 +19,33 @@ Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Pe
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 0 -Type Dword -Force
 
 # Remove task view
-Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name ShowTaskViewButton -Value 0 -Force
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowTaskViewButton" -Value 0 -Force
+
+# Hide desktop icons
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "HideIcons" -Value 1 -Force
+Stop-Process -processName: Explorer # Restart explorer to apply above two changes
 
 # Enable the clipboard history
-Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Clipboard -Name EnableClipboardHistory -Value 1
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Clipboard" -Name EnableClipboardHistory -Value 1
 
 # Set print screen to open snipping tool
-Set-ItemProperty -Path HKCU:\Control Panel\Keyboard -Name PrintScreenKeyForSnippingEnabled -Value 1 -Type Dword
+Set-ItemProperty -Path "HKCU:\Control Panel\Keyboard" -Name PrintScreenKeyForSnippingEnabled -Value 1 -Type Dword
 
-# Change default browser to Chrome
-$originalFile = "C:\Windows\System32\OEMDefaultAssociations.xml"
-$content = Get-Content $originalFile
+# Setup edge redirect - https://github.com/rcmaehl/MSEdgeRedirect/wiki/Deploying-MSEdgeRedirect
+Invoke-WebRequest https://github.com/rcmaehl/MSEdgeRedirect/releases/latest/download/MSEdgeRedirect.exe -OutFile .\MSEdgeRedirect.exe
+Start-Process MSEdgeRedirect.exe -ArgumentList "/silentinstall",".\edge_redirect.ini" -PassThru
+Remove-Item -Path ".\MSEdgeRedirect.exe"
 
-# Replace the Microsoft Edge associations with Google Chrome
-$content = $content.Replace('<Association Identifier="http" ProgId="AppXq0fevzme2pys62n3e0fbqa7peapykr8v" ApplicationName="Microsoft Edge" />', '<Association Identifier="http" ProgId="ChromeHTML" ApplicationName="Google Chrome" />')
-$content = $content.Replace('<Association Identifier="https" ProgId="AppX90nv6nhay5n6a98fnetv7tpk64pp35es" ApplicationName="Microsoft Edge" />', '<Association Identifier="https" ProgId="ChromeHTML" ApplicationName="Google Chrome" />')
-$content = $content.Replace('<Association Identifier=".htm" ProgId="AppX4hxtad77fbk3jkkeerkrm0ze94wjf3s9" ApplicationName="Microsoft Edge" />', '<Association Identifier=".htm" ProgId="ChromeHTML" ApplicationName="Google Chrome" />')
-$content = $content.Replace('<Association Identifier=".html" ProgId="AppX4hxtad77fbk3jkkeerkrm0ze94wjf3s9" ApplicationName="Microsoft Edge" />', '<Association Identifier=".html" ProgId="ChromeHTML" ApplicationName="Google Chrome" />')
+# Close the trash browser :(
+Stop-Process -Name msedge -Force
 
 Set-Content -Path $originalFile -Value $content
 
 # Open useful tabs
-Start-Process "chrome.exe" "https://www.bbc.co.uk/news","--profile-directory="Default""
-Start-Process "chrome.exe" "https://github.com/login","--profile-directory="Default""
-Start-Process "chrome.exe" "https://teams.microsoft.com/","--profile-directory="Default""
-Start-Process "chrome.exe" "https://office.com","--profile-directory="Default""
-
-# Close the trash browser
-Stop-Process -Name msedge -Force
+Start-Process "chrome.exe" "https://www.bbc.co.uk/news"
+Start-Process "chrome.exe" "https://github.com/login"
+Start-Process "chrome.exe" "https://teams.microsoft.com/"
+Start-Process "chrome.exe" "https://office.com"
 
 
 # Easter egg ;)
