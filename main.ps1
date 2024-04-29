5dffc334f9743e8e6259068304cfd117597c7a9a
insolvency <104585393+insolvency@users.noreply.github.com>
2024-04-29 15:27:53 +0100

    Add confirmations for every option in the script

--- main.ps1
+++ main.ps1
@@ -11,67 +11,98 @@ catch {
 }
 }
 
-UnPin-App "Microsoft Edge"
-UnPin-App "Microsoft Store"
-UnPin-App "Mail"
+function Confirmation{
+    param ([string]$text)
+
+    $option = Read-Host $text "[y/n]"
+    return $option -eq "y"
+}
+
+# Unpin unused apps from the taskbar
+if (Confirmation "Unpin unused apps") {
+    UnPin-App "Microsoft Edge"
+    UnPin-App "Microsoft Store"
+    UnPin-App "Mail"
+}
 
 # Turns on dark mode for apps and system
-$themesPersonalise = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize"
-Set-ItemProperty -Path $themesPersonalise -Name "AppsUseLightTheme" -Value 0 -Type Dword
-Set-ItemProperty -Path $themesPersonalise -Name "SystemUsesLightTheme" -Value 0 -Type Dword
+if (Confirmation "Turn on dark mode for apps and system") {
+    $themesPersonalise = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize"
+    Set-ItemProperty -Path $themesPersonalise -Name "AppsUseLightTheme" -Value 0 -Type Dword
+    Set-ItemProperty -Path $themesPersonalise -Name "SystemUsesLightTheme" -Value 0 -Type Dword
+}
 
 $explorer = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+
 # Remove task view
-Set-ItemProperty -Path $explorer -Name "ShowTaskViewButton" -Value 0
+if (Confirmation "Remove task view") {
+    Set-ItemProperty -Path $explorer -Name "ShowTaskViewButton" -Value 0
+}
 
 # Turn on file extensions in File Explorer
-Set-ItemProperty -Path $explorer -Name "HideFileExt" -Value 0
+if (Confirmation "Turn on file extensions in File Explorer") {
+    Set-ItemProperty -Path $explorer -Name "HideFileExt" -Value 0
+}
 
 # Hide desktop icons
-Set-ItemProperty -Path $explorer -Name "HideIcons" -Value 1
+if (Confirmation "Hide desktop icons") {
+    Set-ItemProperty -Path $explorer -Name "HideIcons" -Value 1
+}
 
 # Enable seconds in clock
-Set-ItemProperty -Path $explorer -Name "ShowSecondsInSystemClock" -Value 1 -Force
+if (Confirmation "Enable seconds on clock") {
+    Set-ItemProperty -Path $explorer -Name "ShowSecondsInSystemClock" -Value 1 -Force
+}
 
 # Enable 12 hour time in clock
-Set-ItemProperty -Path $explorer -Name "UseWin32TrayClockExperience" -Value 0 -Force
+if (Confirmation "Enable 12 hour time in clock") {
+    Set-ItemProperty -Path $explorer -Name "UseWin32TrayClockExperience" -Value 0 -Force
+}
 
 # Enable the clipboard history
-Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Clipboard" -Name "EnableClipboardHistory" -Value 1
+if (Confirmation "Enable clipboard history") {
+    Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Clipboard" -Name "EnableClipboardHistory" -Value 1
+}
 
 # Set print screen to open snipping tool
-Set-ItemProperty -Path "HKCU:\Control Panel\Keyboard" -Name "PrintScreenKeyForSnippingEnabled" -Value 1 -Type Dword
+if (Confirmation "Rebind print screen to open snipping tool") {
+    Set-ItemProperty -Path "HKCU:\Control Panel\Keyboard" -Name "PrintScreenKeyForSnippingEnabled" -Value 1 -Type Dword
+}
 
 # Set scroll lines to 7
-Add-Type -TypeDefinition @"
+if (Confirmation "Set scroll lines to 7") {
+    Add-Type -TypeDefinition @"
 using System;
 using System.Runtime.InteropServices;
 
 public class WinAPI {
-	[DllImport("user32.dll", SetLastError = true)]
-	public static extern IntPtr SendMessageTimeout(IntPtr hWnd, int Msg, IntPtr wParam, string lParam, uint fuFlags, uint uTimeout, IntPtr lpdwResult);
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern IntPtr SendMessageTimeout(IntPtr hWnd, int Msg, IntPtr wParam, string lParam, uint fuFlags, uint uTimeout, IntPtr lpdwResult);
 
-	[DllImport("user32.dll", SetLastError = true)]
-	public static extern bool SystemParametersInfo(uint uiAction, uint uiParam, uint pvParam, uint fWinIni);
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern bool SystemParametersInfo(uint uiAction, uint uiParam, uint pvParam, uint fWinIni);
 }
 "@
 
-$scrollSpeed = 7
-Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "WheelScrollLines" -Value $scrollSpeed
-[WinAPI]::SystemParametersInfo(0x0069, $scrollSpeed, 0, 2)
-[WinAPI]::SendMessageTimeout(0xffff, 0x1a, [IntPtr]::Zero, "Environment", 2, 5000, [IntPtr]::Zero)
+    $scrollSpeed = 7
+    Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "WheelScrollLines" -Value $scrollSpeed
+    [WinAPI]::SystemParametersInfo(0x0069, $scrollSpeed, 0, 2)
+    [WinAPI]::SendMessageTimeout(0xffff, 0x1a, [IntPtr]::Zero, "Environment", 2, 5000, [IntPtr]::Zero)
+}
 
 # Setup edge redirect - https://github.com/rcmaehl/MSEdgeRedirect/wiki/Deploying-MSEdgeRedirect
-Invoke-WebRequest "https://github.com/rcmaehl/MSEdgeRedirect/releases/latest/download/MSEdgeRedirect.exe" -OutFile .\MSEdgeRedirect.exe
-Invoke-WebRequest "https://raw.githubusercontent.com/likes-gay/win-config/main/edge_redirect.ini" -OutFile .\edge_redirect.ini
-Start-Process "MSEdgeRedirect.exe" -ArgumentList "/silentinstall",".\edge_redirect.ini" -PassThru
-Remove-Item -Path ".\edge_redirect.ini"
-Remove-Item -Path ".\MSEdgeRedirect.exe"
+if (Confirmation "Install and configure MSEdgeRedirect") {
+    Invoke-WebRequest "https://github.com/rcmaehl/MSEdgeRedirect/releases/latest/download/MSEdgeRedirect.exe" -OutFile .\MSEdgeRedirect.exe
+    Invoke-WebRequest "https://raw.githubusercontent.com/likes-gay/win-config/main/edge_redirect.ini" -OutFile .\edge_redirect.ini
+    Start-Process "MSEdgeRedirect.exe" -ArgumentList "/silentinstall",".\edge_redirect.ini" -PassThru
+    Remove-Item -Path ".\edge_redirect.ini"
+    Remove-Item -Path ".\MSEdgeRedirect.exe"
+}
 
 try {
-	Stop-Process -Name msedge -Force
+    Stop-Process -Name msedge -Force
 } catch {
-	Write-Output "Microsoft Edge is already shut"
+    Write-Output "Microsoft Edge is already shut"
 }
 
 try {
