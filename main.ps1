023a8ab40f4e821c960c94a3e51e6caf78eedcdc
Elliot <140021772+Emorgan12@users.noreply.github.com>
2025-03-18 09:21:56 +0000

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -320,7 +320,7 @@ if ($configFile."Install-gh-cli") {
 
 # Install UV (Python PIP replacement https://github.com/astral-sh/uv)
 if ($configFile."Install-UV") {
-	powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex
+	powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
 	if ($configFile."Campus-UV-PIP-Fix") {
  		New-Item -ItemType Directory -Force -Path "$Env:APPDATA\uv"
 		Invoke-WebRequest "https://github.com/likes-gay/win-config/raw/refs/heads/main/uv.toml" -OutFile "$Env:APPDATA\uv\uv.toml"
